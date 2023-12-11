85e050950f5c68c9b134dff9906ae8381c2810c7
Ryan Faulhaber <ryf@sent.as>
2023-12-10 21:03:43 -0500

    attempt on day 4 part 2

--- 4rkt/day4.rkt
+++ 4rkt/day4.rkt
@@ -25,6 +25,10 @@
 (define (calculate-score numbers)
   (if (empty? numbers) 0 (expt 2 (- (length numbers) 1))))
 
+(define (get-winning-count card)
+  (let ([winning-numbers (car card)] [game-numbers (cadr card)])
+    (length (filter (lambda (n) (member n winning-numbers)) game-numbers))))
+
 (define (solve part input)
   (if (= part 1) (solve-part-1 input) (solve-part-2 input)))
 
@@ -32,7 +36,14 @@
   (apply + (map get-score-for-card (parse-input input))))
 
 (define (solve-part-2 input)
-  10)
+  (let* ([cards (parse-input input)]
+         [cards-by-index (map cons (range 0 (length cards)) cards)]
+         [new-cards (range 0 (length cards))])
+
+    (for ([card-by-index cards-by-index])
+      (let ([card (cdr card-by-index)] [index (car card-by-index)])
+        (set! new-cards (append new-cards (range index (+ index (get-winning-count card)))))))
+    (length new-cards)))
 
 (module+ test
   (require rackunit)
@@ -44,4 +55,9 @@
 
   (check-equal? (get-score-for-card '((41 48 83 86 17) (83 86 6 31 17 9 48 53))) 8)
 
-  (check-equal? (solve-part-1 "./sample.txt") 13))
+  (check-equal? (solve-part-1 "./sample.txt") 13)
+
+  (check-equal? (get-winning-count '((41 48 83 86 17) (83 86 6 31 17 9 48 53))) 4)
+  (check-equal? (get-winning-count '((13 32 20 16 61) (61 30 68 82 17 32 24 19))) 2)
+
+  (check-equal? (solve-part-2 "./sample.txt") 30))
