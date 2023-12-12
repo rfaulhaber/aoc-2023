306c5a650f3cbb6b3308b3118de20a8c5ebfe91b
Ryan Faulhaber <ryf@sent.as>
2023-12-11 21:26:33 -0500

    day 4 part 2

--- 4rkt/day4.rkt
+++ 4rkt/day4.rkt
@@ -29,6 +29,9 @@
   (let ([winning-numbers (car card)] [game-numbers (cadr card)])
     (length (filter (lambda (n) (member n winning-numbers)) game-numbers))))
 
+(define (evaluate-card card index)
+  (range (+ index 1) (+ 1 index (get-winning-count card))))
+
 (define (solve part input)
   (if (= part 1) (solve-part-1 input) (solve-part-2 input)))
 
@@ -38,12 +41,17 @@
 (define (solve-part-2 input)
   (let* ([cards (parse-input input)]
          [cards-by-index (map cons (range 0 (length cards)) cards)]
-         [new-cards (range 0 (length cards))])
+         [winning-numbers (map (lambda (pair) (evaluate-card (cdr pair) (car pair))) cards-by-index)]
+         [new-cards (make-list (length cards) 1)])
 
-    (for ([card-by-index cards-by-index])
-      (let ([card (cdr card-by-index)] [index (car card-by-index)])
-        (set! new-cards (append new-cards (range index (+ index (get-winning-count card)))))))
-    (length new-cards)))
+    ;; thank you reddit
+    ;; this was not very intuitive to me
+    (for* ((i (length cards))
+           (j (range (+ i 1) (+ i 1 (length (list-ref winning-numbers i))))))
+
+      (set! new-cards (list-set new-cards j (+ (list-ref new-cards i) (list-ref new-cards j)))))
+
+    (apply + new-cards)))
 
 (module+ test
   (require rackunit)
@@ -59,5 +67,11 @@
 
   (check-equal? (get-winning-count '((41 48 83 86 17) (83 86 6 31 17 9 48 53))) 4)
   (check-equal? (get-winning-count '((13 32 20 16 61) (61 30 68 82 17 32 24 19))) 2)
+
+  (check-equal? (evaluate-card '((41 48 83 86 17) (83 86 6 31 17 9 48 53)) 0) '(1 2 3 4))
+
+  (check-equal? (evaluate-card '((13 32 20 16 61)  (61 30 68 82 17 32 24 19)) 1) '(2 3))
+
+  (check-equal? (evaluate-card '((87 83 26 28 32) (88 30 70 12 93 22 82 36)) 4) '())
 
   (check-equal? (solve-part-2 "./sample.txt") 30))
